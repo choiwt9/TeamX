a9724871d8cc540692968c126e61d4ee9b7f8efd
hailey-516 <leehi516@naver.com>
2024-10-23 16:03:35 +0900

    build: 디렉토리 구조 설정 및 초기 설정 작업
    - 디렉토리 구조 설정
    
    - 라이브러리 추가
    
    - properties 추가 및 db 관련 설정
    
    - index 파일 추가

---
+++ src/test/java/com/teamx/exsite/ExsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamx.exsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
