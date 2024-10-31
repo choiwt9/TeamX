16914827fb54657a257bbc7211c552a5187b4e11
rsvprzdz <rsvprzdz@naver.com>
2024-10-31 15:32:05 +0900

    chore: gitignore 파일 수정 및 bin 폴더 형상관리 제외 처리

--- exsite/src/main/java/com/teamx/exsite/controller/community/CommunityController.java
+++ exsite/src/main/java/com/teamx/exsite/controller/community/CommunityController.java
@@ -6,22 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CommunityController {
 
-	@GetMapping("/communityList")
+	@GetMapping("/community/list")
 	public String CommunityList() {
 		return "community/communityList"; // templates/community/communityList.html 파일을 반환
 	}
 	
-	@GetMapping("/communityWrite")
+	@GetMapping("/community/write")
 	public String CommunityWrite() {
 		return "community/communityWrite"; // templates/community/communityWrite.html 파일을 반환
 	}
 	
-	@GetMapping("/communityPost")
+	@GetMapping("/community/post")
 	public String CommunityPost() {
 		return "community/communityPost"; // templates/community/communityPost.html 파일을 반환
 	}
 	
-	@GetMapping("/communityEdit")
+	@GetMapping("/community/edit")
 	public String CommunityEdit() {
 		return "community/communityEdit";
 	}
