0612de6d14c63c3af3b333e20256632e8b8a9ab9
jeonghyunkim89 <dool6137@naver.com>
2024-11-07 09:37:01 +0900

    feat: 전시목록조회,전시상세조회,행사목록조회,행사상세조회 기능구현, 좋아요기능 진행중

---
+++ exsite/src/main/java/com/teamx/exsite/model/exhibition/vo/ExhibitionEvent.java
@@ -0,0 +1,22 @@
+package com.teamx.exsite.model.exhibition.vo;
+
+import lombok.Data;
+
+@Data
+public class ExhibitionEvent {
+
+	private int exhibitionNo;
+	private String title;
+	private String place;
+	private String mainImg;
+	private String exDate;
+	private String codeName;
+	private String isFree;
+	private String useFee;
+	private String strtDate;
+	private String endDate;
+	private String detailImgUrl;
+	private int userNo;
+	private int likeCount; // 좋아요 개수 추가
+	
+}
---
+++ exsite/src/main/java/com/teamx/exsite/model/exhibition/vo/ExhibitionLike.java
@@ -0,0 +1,11 @@
+package com.teamx.exsite.model.exhibition.vo;
+
+import lombok.Data;
+
+@Data
+public class ExhibitionLike {
+
+	private int userNo;
+	private int exhibitionNo;
+	
+}
