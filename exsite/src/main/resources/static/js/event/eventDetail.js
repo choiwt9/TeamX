d34e83c74884b4af8907fde155589fe2f2410791
choiwontak <wontak9@gmail.com>
2024-11-07 16:11:14 +0900

    docs:eventDetail.html,exhibition.html, exhibition.css 수정  eventDetail.js, exhibitionDetail.js jquery 추가 reviewCheck html, css, js 파일 삭제

--- exsite/src/main/resources/static/js/event/eventDetail.js
+++ exsite/src/main/resources/static/js/event/eventDetail.js
@@ -53,5 +53,17 @@ window.onload = () => {
         this.style.color = '#0B9B9B'; 
         document.getElementById('exhibition-details-button').style.color = ''; 
     });
-
+    $(document).ready(function () {
+        $("#more").click(function () {
+            const $textContent = $("#review-content");
+            $textContent.toggleClass("expanded");
+    
+            if ($textContent.hasClass("expanded")) {
+                $(this).text("간략히");
+            } else {
+                $(this).text("더보기");
+            }
+        });
+    });
+    
 }
